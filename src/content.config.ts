aed7db5d9a4191c3b72971787be8d2190680e249
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2026-03-14 13:55:46 +0530

    first commit

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,6 +14,7 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
+			previewHeroImage: z.optional(image()),
 			authorImage: z.optional(image()),
 			authorName: z.string().optional(),
 			authorDesignation: z.string().optional(),
